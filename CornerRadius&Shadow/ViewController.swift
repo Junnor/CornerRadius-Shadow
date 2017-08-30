2398ef02a705031e1031025b43f463f2794e75a7
朱邓全 <zhudengquan@nyato.com>
2017-08-30 17:42:21 +0800

    add corner view test

--- CornerRadius&Shadow/ViewController.swift
+++ CornerRadius&Shadow/ViewController.swift
@@ -17,6 +17,76 @@ class ViewController: UIViewController {
 }
 
 
+@IBDesignable
+class CornerShadowView: UIView {
+    
+    @IBInspectable
+    var cornerRadius: CGFloat = 22 {
+        didSet {
+            layoutIfNeeded()
+        }
+    }
+    
+    @IBInspectable
+    var shadowRadius: CGFloat = 3 {
+        didSet {
+            layoutIfNeeded()
+        }
+    }
+    
+    @IBInspectable
+    var shadowOpacity: Float = 0.9 {
+        didSet {
+            layoutIfNeeded()
+        }
+    }
+    
+    @IBInspectable
+    var shadowOffset: CGSize = CGSize(width: 0, height: 0) {
+        didSet {
+            layoutIfNeeded()
+        }
+    }
+    
+    @IBInspectable
+    var shadowColor: UIColor = UIColor.black {
+        didSet {
+            layoutIfNeeded()
+        }
+    }
+    
+    @IBInspectable
+    var fillColor: UIColor = UIColor.black {
+        didSet {
+            layoutIfNeeded()
+        }
+    }
+    
+    private var shadowLayer: CAShapeLayer!
+    
+    override func draw(_ rect: CGRect) {
+        super.draw(rect)
+        
+        layer.cornerRadius = cornerRadius
+        
+        if shadowLayer == nil {
+            shadowLayer = CAShapeLayer()
+            layer.insertSublayer(shadowLayer, at: 0)
+            
+        }
+        
+        shadowLayer.fillColor = fillColor.cgColor
+        shadowLayer.shadowColor = shadowColor.cgColor
+        shadowLayer.shadowOffset = shadowOffset
+        shadowLayer.shadowRadius = shadowRadius
+        shadowLayer.shadowOpacity = shadowOpacity
+        shadowLayer.path = UIBezierPath(roundedRect: bounds, cornerRadius: cornerRadius).cgPath
+        shadowLayer.shadowPath = shadowLayer.path
+    }
+    
+}
+
+
 /**
   If inherit like:
   CornerShadowView: UIView {}
